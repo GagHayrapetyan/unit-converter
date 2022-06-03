d38d27c4fb23b55a2bb5f6aab83df0e452cd21f8
gag <gagik.nh@grovf.com>
2022-06-03 15:23:25 +0400

    rename

--- example.cpp
+++ example.cpp
@@ -4,7 +4,7 @@
 
 
 int main() {
-    auto c = unit_converter::Converter("km", "m");
+    auto c = unit_converter::Converter("km/h", "m/s");
 
     std::cout << c.convert(1) << std::endl;
     std::cout << c.convert(1, true) << std::endl;
