1e2c80ce32aafad0e5740e62cbdcb51746427a7e
Gagik <gagik.hayrapetyan.00@gmail.com>
2022-06-03 13:48:04 +0400

    converter

---
+++ converter.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Gag Hayrapetyan on 03.06.2022.
+//
+#include "converter.h"
+
+namespace unit_converter {
+
+    Converter::Converter(std::string from, std::string to)
+            : _from_str(from),
+              _to_str(to),
+              _from({}),
+              _to({}) {
+        _from = UnitParser::parse(_from_str);
+        _to = UnitParser::parse(_to_str);
+
+        if (_from.is_same_dimension(_to)) {
+            throw std::runtime_error("Units are not of the same dimension !");
+        }
+    }
+
+    std::ostream &operator<<(std::ostream &os, const Converter &obj) {
+        return os << obj._from_str << " -> " << obj._to_str << std::endl;
+    }
+
+    double Converter::convert(double value) {
+        _from.convert(value, Unit::Direction::TO_SI);
+        _to.convert(value, Unit::Direction::FROM_SI);
+
+        return value;
+    }
+}
---
+++ converter.h
@@ -0,0 +1,34 @@
+//
+// Created by Gag Hayrapetyan on 03.06.2022.
+//
+
+#include <iostream>
+#include <string>
+
+#include "parser.h"
+#include "unit.h"
+
+#ifndef UNIT_CONVERTER_CONVERTER_H
+#define UNIT_CONVERTER_CONVERTER_H
+
+namespace unit_converter {
+
+    class Converter {
+    public:
+        Converter(std::string from, std::string to);
+
+        friend std::ostream &operator<<(std::ostream &os, const Converter &obj);
+
+        double convert(double value);
+
+    private:
+        std::string _from_str;
+        std::string _to_str;
+        MultiUnit _from;
+        MultiUnit _to;
+
+    };
+
+}
+
+#endif //UNIT_CONVERTER_CONVERTER_H
